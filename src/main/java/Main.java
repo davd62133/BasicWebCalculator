42f17f26ca4603a377ce6799c3562941fbab6d3f
David <david.rodriguez-a@mail.escuelaing.edu.co>
2019-02-12 10:17:41 -0500

    minor fix 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -138,3 +138,25 @@ public class Main {
     }
 
 }
+
+class JSON {
+    public static Double max;
+    public static Double min;
+    public static Double sum;
+    public static Double mult;
+    public static LinkedList<Double> list;
+
+
+
+    public static String getJSON() {
+        String temp =
+                "{"+
+                        "max : " + max +
+                        ", min : " + min +
+                        ", sum : " + sum +
+                        ", mult : " + mult +
+                        ", list : " + list +
+                        "}";
+        return temp;
+    }
+}
